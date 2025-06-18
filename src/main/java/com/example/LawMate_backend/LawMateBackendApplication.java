effb292181ccc7b7cf93f080b1737f5388bb2bed
김정현 <richard.kim146@gmail.com>
2025-06-18 13:45:15 +0900

    initial

---
+++ src/main/java/com/example/LawMate_backend/LawMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.LawMate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawMateBackendApplication.class, args);
+	}
+
+}
