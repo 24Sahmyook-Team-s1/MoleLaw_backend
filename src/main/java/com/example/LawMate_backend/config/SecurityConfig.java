e038fcfb59f82a75d5fe11c6da093129c85d454a
김정현 <richard.kim146@gmail.com>
2025-06-18 14:20:17 +0900

    initial

---
+++ src/main/java/com/example/LawMate_backend/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.LawMate_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (API 서버일 경우 보통 비활성화)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll() // Swagger 허용
+                        .anyRequest().permitAll()  // 모든 요청 허용
+                );
+        return http.build();
+    }
+}
