a27ce2bc4535bee6f64bbdb4790a5e9911f8dad4
김정현 <richard.kim146@gmail.com>
2025-06-20 10:08:19 +0900

    refactor: lawmate-> molelaw 이름 리펙터링

--- src/main/java/com/MoleLaw_backend/MoleLawBackendApplication.java
+++ src/main/java/com/MoleLaw_backend/MoleLawBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LawMateBackendApplication {
+public class MoleLawBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LawMateBackendApplication.class, args);
+		SpringApplication.run(MoleLawBackendApplication.class, args);
 	}
 
 }
