bbc8d85a056c1a4d0f4559b19d7fa28e615d51f3
WhiteMilk357 <w3mil57@gmail.com>
2025-06-23 12:39:33 +0900

    법 이름 받아오기

---
+++ src/main/java/com/MoleLaw_backend/controller/LawSearchController.java
@@ -0,0 +1,23 @@
+package com.MoleLaw_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import com.MoleLaw_backend.LawSearchService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/laws")
+public class LawSearchController {
+
+    private final LawSearchService lawSearchService;
+
+    @GetMapping("/search")
+    public ResponseEntity<String> searchLaws(@RequestParam String keyword) {
+        String resultXml = lawSearchService.searchLawByKeyword(keyword);
+        return ResponseEntity.ok(resultXml);
+    }
+}
