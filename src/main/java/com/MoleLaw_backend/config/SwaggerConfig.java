8d1337b54a337e62cf252e2839e23e547df42ccd
김정현 <richard.kim146@gmail.com>
2025-06-26 17:10:32 +0900

    fix: application.yml 수정

--- src/main/java/com/MoleLaw_backend/config/SwaggerConfig.java
+++ src/main/java/com/MoleLaw_backend/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.MoleLaw_backend.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -15,14 +17,22 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
         scheme = "bearer",
         bearerFormat = "JWT"
 )
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://team-moleback.store", description = "molelaw 배포 백엔드 서버"),
+                @Server(url = "http://localhost:8080", description = "개발 로컬 환경")
+        }
+)
+
+
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("LawMate API")
-                        .description("LawMate 백엔드 API 문서입니다.")
+                        .title("MoleLaw API")
+                        .description("MoleLaw 백엔드 API 문서입니다.")
                         .version("v1.0"))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
     }
