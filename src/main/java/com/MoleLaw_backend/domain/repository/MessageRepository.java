a2b7ab54c3c8cbfb30263cee364dfd8f9ce4ab73
김정현 <richard.kim146@gmail.com>
2025-07-01 16:48:34 +0900

    fix: 충돌 해결

--- src/main/java/com/MoleLaw_backend/domain/repository/MessageRepository.java
+++ src/main/java/com/MoleLaw_backend/domain/repository/MessageRepository.java
@@ -1,6 +1,5 @@
 package com.MoleLaw_backend.domain.repository;
 
-import com.MoleLaw_backend.domain.entity.ChatRoom;
 import com.MoleLaw_backend.domain.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -14,5 +13,5 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
     long countByChatRoomId(Long chatRoomId);
 
-    List<Message> findByChatRoomOrderByCreatedAtAsc(ChatRoom room);
+
 }
