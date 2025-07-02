47b90c7871ea4ca4d834c12e937a0783bf2dedf5
김정현 <richard.kim146@gmail.com>
2025-07-02 15:20:08 +0900

    fix: 수정 완료 - 채팅방 데이터베이스에 유저id 포함 안되던 문제 수정

--- src/main/java/com/MoleLaw_backend/controller/ChatController.java
+++ src/main/java/com/MoleLaw_backend/controller/ChatController.java
@@ -1,12 +1,9 @@
 package com.MoleLaw_backend.controller;
 
-import com.MoleLaw_backend.domain.entity.User;
 import com.MoleLaw_backend.dto.*;
 import com.MoleLaw_backend.service.ChatService;
 import com.MoleLaw_backend.service.security.CustomUserDetails;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +17,8 @@ public class ChatController {
     private final ChatService chatService;
 
     @GetMapping
-    public List<ChatRoomResponse> getMyRooms(@AuthenticationPrincipal User user) {
-        return chatService.getMyChatRooms(user);
+    public List<ChatRoomResponse> getMyRooms(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return chatService.getMyChatRooms(userDetails.getUser());
     }
 
     @GetMapping("/{roomId}")
@@ -30,18 +27,16 @@ public class ChatController {
         return chatService.getMessages(userDetails.getUser(), roomId);
     }
 
-
     @PostMapping("/{roomId}/messages")
-    public void ask(@AuthenticationPrincipal User user,
+    public void ask(@AuthenticationPrincipal CustomUserDetails userDetails,
                     @PathVariable Long roomId,
                     @RequestBody MessageRequest request) {
-        chatService.askQuestion(user, roomId, request);
+        chatService.askQuestion(userDetails.getUser(), roomId, request);
     }
 
     @PostMapping("/first-message")
-    public List<MessageResponse> createAndAsk(@AuthenticationPrincipal User user,
+    public List<MessageResponse> createAndAsk(@AuthenticationPrincipal CustomUserDetails userDetails,
                                               @RequestBody FirstMessageRequest request) {
-        return chatService.createRoomAndAsk(user, request);
+        return chatService.createRoomAndAsk(userDetails.getUser(), request);
     }
-
 }
