3737d2f87b2e3fdf37754e06b8ce7186a17bd2ad
김정현 <richard.kim146@gmail.com>
2025-07-04 11:08:54 +0900

    fix: 소관부처로 필터링한 법령 검색 - 소관 부처 맵핑 추가 - 소관부처 포함 법령검색 로직으로 변경

---
+++ src/main/java/com/MoleLaw_backend/util/MinistryCodeMapper.java
@@ -0,0 +1,123 @@
+package com.MoleLaw_backend.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinistryCodeMapper {
+
+    private static final Map<String, String> ministryCodeMap = new HashMap<>();
+
+    static {
+        ministryCodeMap.put("10·29이태원참사진상규명과재발방지를위한특별조사위원회", "1834600");
+        ministryCodeMap.put("5.18민주화운동진상규명조사위원회", "1075000");
+        ministryCodeMap.put("가습기살균제사건과4.16세월호참사특별조사위원회", "1074601");
+        ministryCodeMap.put("감사원", "1040000");
+        ministryCodeMap.put("개인정보보호위원회", "1790365");
+        ministryCodeMap.put("게임물등급위원회", "B552557");
+        ministryCodeMap.put("경찰청", "1320000");
+        ministryCodeMap.put("고용노동부", "1492000");
+        ministryCodeMap.put("고위공직자범죄수사처", "1790364");
+        ministryCodeMap.put("금융위원회 공적자금관리위원회사무국", "1160100");
+        ministryCodeMap.put("공정거래위원회", "1130000");
+        ministryCodeMap.put("과학기술정보통신부", "1721000");
+        ministryCodeMap.put("관세청", "1220000");
+        ministryCodeMap.put("교육부", "1342000");
+        ministryCodeMap.put("국가교육위원회", "1813266");
+        ministryCodeMap.put("국가교육회의", "1072600");
+        ministryCodeMap.put("국가균형발전위원회", "1073500");
+        ministryCodeMap.put("국가보훈부", "1830000");
+        ministryCodeMap.put("국가안보실", "1025000");
+        ministryCodeMap.put("국가안전보장회의", "00040");
+        ministryCodeMap.put("국가유산청", "1833100");
+        ministryCodeMap.put("국가인권위원회", "1620000");
+        ministryCodeMap.put("국가정보원", "1030000");
+        ministryCodeMap.put("질병관리청 국립보건연구원", "1790387");
+        ministryCodeMap.put("해양수산부 국립수산과학원", "1192000");
+        ministryCodeMap.put("국립전파연구원", "1721000");
+        ministryCodeMap.put("국무조정실", "1092000");
+        ministryCodeMap.put("국무총리비서실", "1103000");
+        ministryCodeMap.put("국무총리실", "1101000");
+        ministryCodeMap.put("국민권익위원회", "1140100");
+        ministryCodeMap.put("국민안전처", "1750000");
+        ministryCodeMap.put("국방부", "1290000");
+        ministryCodeMap.put("국세청", "1210000");
+        ministryCodeMap.put("국토교통부", "1613000");
+        ministryCodeMap.put("국회", "9700000");
+        ministryCodeMap.put("국회 국회도서관", "9700000");
+        ministryCodeMap.put("국회 국회사무처", "9700000");
+        ministryCodeMap.put("국회 국회예산정책처", "9700000");
+        ministryCodeMap.put("국회 국회입법조사처", "9700000");
+        ministryCodeMap.put("군의문사진상규명위원회", "1561000");
+        ministryCodeMap.put("금융위원회", "1160100");
+        ministryCodeMap.put("기상청", "1360000");
+        ministryCodeMap.put("기획재정부", "1051000");
+        ministryCodeMap.put("농림수산식품부", "1541000");
+        ministryCodeMap.put("농림축산식품부", "1543000");
+        ministryCodeMap.put("농촌진흥청", "1390000");
+        ministryCodeMap.put("대검찰청", "1280000");
+        ministryCodeMap.put("대법원", "9740000");
+        ministryCodeMap.put("대통령경호처", "1021100");
+        ministryCodeMap.put("대통령비서실", "1015000");
+        ministryCodeMap.put("대통령실", "1011000");
+        ministryCodeMap.put("문화체육관광부", "1371000");
+        ministryCodeMap.put("미래창조과학부", "1710000");
+        ministryCodeMap.put("민주평화통일자문회의사무처", "1081000");
+        ministryCodeMap.put("방송통신위원회", "1570100");
+        ministryCodeMap.put("방위사업청", "1690000");
+        ministryCodeMap.put("법무부", "1270000");
+        ministryCodeMap.put("대법원 법원행정처", "9740000");
+        ministryCodeMap.put("법제처", "1170000");
+        ministryCodeMap.put("병무청", "1300000");
+        ministryCodeMap.put("보건복지부", "1352000");
+        ministryCodeMap.put("비상기획위원회", "B000000");
+        ministryCodeMap.put("문화체육관광부 사행산업통합감독위원회", "1371000");
+        ministryCodeMap.put("문화체육관광부 사행산업통합감독위원회 사행산업통합감독위원회사무처", "1371000");
+        ministryCodeMap.put("산림청", "1400000");
+        ministryCodeMap.put("산업통상자원부", "1450000");
+        ministryCodeMap.put("새만금개발청", "1730000");
+        ministryCodeMap.put("소방청", "1661000");
+        ministryCodeMap.put("수원도시공사", "B000000");
+        ministryCodeMap.put("식품의약품안전처", "1471000");
+        ministryCodeMap.put("여성가족부", "1383000");
+        ministryCodeMap.put("문화체육관광부 대한민국예술원 예술원사무국", "1371000");
+        ministryCodeMap.put("외교부", "1262000");
+        ministryCodeMap.put("우주항공청", "1834100");
+        ministryCodeMap.put("원자력안전위원회", "1079960");
+        ministryCodeMap.put("인사혁신처", "1760000");
+        ministryCodeMap.put("일제강점하강제동원피해진상규명위원회", "1655000");
+        ministryCodeMap.put("재외동포청", "1832000");
+        ministryCodeMap.put("국민권익위원회 정부합동민원센터", "1140100");
+        ministryCodeMap.put("조달청", "1230000");
+        ministryCodeMap.put("중소벤처기업부", "1421000");
+        ministryCodeMap.put("중앙선거관리위원회", "9760000");
+        ministryCodeMap.put("환경부 중앙환경분쟁조정피해구제위원회", "1480000");
+        ministryCodeMap.put("진실화해를위한과거사정리위원회", "1680000");
+        ministryCodeMap.put("질병관리청", "1790387");
+        ministryCodeMap.put("친일반민족행위자재산조사위원회", "1560100");
+        ministryCodeMap.put("친일반민족행위진상규명위원회", "1560010");
+        ministryCodeMap.put("태평양전쟁전후국외강제동원희생자지원위원회", "1655500");
+        ministryCodeMap.put("통계청", "1240000");
+        ministryCodeMap.put("통일부", "1250000");
+        ministryCodeMap.put("특임장관실", "1091000");
+        ministryCodeMap.put("특허청", "1430000");
+        ministryCodeMap.put("해양경찰청", "1532000");
+        ministryCodeMap.put("해양수산부", "1192000");
+        ministryCodeMap.put("행정안전부", "1741000");
+        ministryCodeMap.put("행정자치부", "1740000");
+        ministryCodeMap.put("행정중심복합도시건설청", "1670000");
+        ministryCodeMap.put("헌법재판소", "9750000");
+        ministryCodeMap.put("환경부", "1480000");
+
+    }
+
+    public static String getCode(String ministryName) {
+        return ministryCodeMap.getOrDefault(ministryName, null);
+    }
+
+    public static boolean contains(String ministryName) {
+        return ministryCodeMap.containsKey(ministryName);
+    }
+
+}
+
+
